8577dbb90f52148ce6d9fb3d45f5f714a80c2e29
ngclam <ngclam1711@gmail.com>
2026-06-24 15:49:37 +0700

    service api and module level
    (cherry picked from commit bb8a68a28fda87b8aaba085624f1177b5d19d094)

---
+++ services/modules/src/education/level/level.service.ts
@@ -0,0 +1,117 @@
+import { useTranslation } from "react-i18next";
+import { useQueryClient } from "@tanstack/react-query";
+import {
+  useQueryAdapter,
+  useMutationAdapter,
+} from "@tera/commons/hooks/queryAdapter";
+import { LevelAPI } from "@tera/api";
+import {
+  CreatePayload,
+  DetailPayload,
+  ListPayload,
+  UpdatePayload,
+} from "@tera/api/_interface";
+
+// QUERY
+export const useLevelList = (payload: ListPayload) => {
+  return useQueryAdapter({
+    queryKey: ["level", "list", payload.params],
+    queryFn: () => LevelAPI.getList(payload),
+    keepPreviousData: true,
+  });
+};
+
+export const useLevelDetail = (payload: DetailPayload) => {
+  return useQueryAdapter({
+    queryKey: ["level", "detail", payload.id],
+    queryFn: () => LevelAPI.getDetail(payload),
+    enabled: !!payload.id,
+  });
+};
+
+// MUTATION
+export const useLevelCreate = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutationAdapter({
+    mutationFn: (payload: CreatePayload) => LevelAPI.create(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["level", "list"] });
+    },
+    onError: (error) => {
+      console.error(t("common.error_message"), error);
+    },
+  });
+};
+
+export const useLevelUpdate = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutationAdapter({
+    mutationFn: (payload: UpdatePayload) => LevelAPI.update(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["level", "list"] });
+      queryClient.invalidateQueries({ queryKey: ["level", "detail"] });
+    },
+    onError: (error) => {
+      console.error(t("common.error_message"), error);
+    },
+  });
+};
+
+export const useUpsertLevel = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutationAdapter({
+    mutationFn: (payload: UpdatePayload) => {
+      if (payload?.id) return LevelAPI.update(payload);
+      return LevelAPI.create(payload);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["level", "list"] });
+    },
+    onError: (error) => {
+      console.error(t("common.error_message"), error);
+    },
+  });
+};
+
+export const useLevelSuspend = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutationAdapter({
+    mutationFn: (payload: UpdatePayload) => LevelAPI.suspend(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["level", "list"] });
+      queryClient.invalidateQueries({ queryKey: ["level", "detail"] });
+    },
+    onError: (error) => {
+      console.error(t("common.error_message"), error);
+    },
+  });
+};
+
+export const useLevelRestore = () => {
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  return useMutationAdapter({
+    mutationFn: (payload: UpdatePayload) => LevelAPI.restore(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["level", "list"] });
+      queryClient.invalidateQueries({ queryKey: ["level", "detail"] });
+    },
+    onError: (error) => {
+      console.error(t("common.error_message"), error);
+    },
+  });
+};
+
+export const LevelService = {
+  useLevelList,
+  useLevelDetail,
+  useLevelCreate,
+  useLevelUpdate,
+  useUpsertLevel,
+  useLevelSuspend,
+  useLevelRestore,
+};
